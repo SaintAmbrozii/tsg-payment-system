bc07e15a6a29866f27bf42fd8c6bb89854c9a2b3
Samsonov Dmitrii <samdim2011@mail.ru>
2023-12-05 13:12:06 +0700

    add ssl cert to service

--- src/main/java/com/example/tsgpaymentsystem/TsgPaymentSystemApplication.java
+++ src/main/java/com/example/tsgpaymentsystem/TsgPaymentSystemApplication.java
@@ -1,7 +1,9 @@
 package com.example.tsgpaymentsystem;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class TsgPaymentSystemApplication {
@@ -9,5 +11,9 @@ public class TsgPaymentSystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(TsgPaymentSystemApplication.class, args);
     }
+
+
+
+
 
 }
